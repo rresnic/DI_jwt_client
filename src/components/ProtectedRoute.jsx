01692a6bfe44a5e1145332da1ff229bf35f86c02
Reuven Resnicoff <yresnicoff@gmail.com>
2025-01-20 10:40:34 +0200

    added loading to ProtectedRoute component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,11 +4,15 @@ import { useEffect } from "react";
 
 const ProtectedRoute = ({children}) => {
     const location = useLocation();
-    const {refreshAuth, isAuthenticated } = useAuth();
+    const {refreshAuth, loading, isAuthenticated } = useAuth();
     console.log(isAuthenticated)
     useEffect(()=> {
         refreshAuth();
     }, [])
+
+    if(loading) {
+        return <div className="loading">Loading...</div>
+    }
     if(!isAuthenticated) {
         console.log("not authenticated")
         return <Navigate to={"/login"} state={{from:location}} replace />
